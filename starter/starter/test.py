106dbeca4a61213a37e6f6520ba0aecefe230768
Yilun Zhang <yilun.zhang@rhsmith.umd.edu>
2022-02-28 11:12:41 -0500

    update test_ml

--- starter/starter/test.py
+++ starter/starter/test.py
@@ -1,5 +1,8 @@
-from model import *
+from starter.starter.model import *
+from starter.starter.data import *
 import unittest
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def test_train_model(train_model):
     clf = train_model(X_train,y_train)
@@ -14,19 +17,28 @@ def test_compute_model_metrics(compute_model_metrics):
     assert None not in [score1, score2, score3]
 
 if __name__ == '__main__':
-    data = pd.read_csv("./starter/data/census_cleaned.csv")
+    data = pd.read_csv("./data/census_cleaned.csv")
     data = data.drop(columns=['Unnamed: 0', 'index'])
     train, test = train_test_split(data, test_size=0.20)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country"
+    ]
     X_train, y_train, encoder, lb = process_data(
         train, categorical_features=cat_features, label="salary", training=True
     )
     test_train_model(train_model)
-    model = train_model
+    model = train_model(X_train,y_train)
     X_test, y_test, _, _ = process_data(
         test, encoder=encoder, lb=lb, categorical_features=cat_features, label="salary", training=False
     )
     y_pred = inference(model, X_test)
     test_inference(inference)
     compute_model_metrics(y_test, y_pred)
-    y_pred = inference(model, X_test)
     test_compute_model_metrics(compute_model_metrics)
